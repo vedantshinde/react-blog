153418a3b22e092a6f7a4ed76d1af442a0b238a6
Vedant Shinde <vedantshinde101@gmail.com>
2021-08-19 12:33:00 +0530

    Add Responsive Navigation menu

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {Link} from 'react-router-dom';
+import { Avatar } from 'antd';
 
 const navLinks = [
     {
@@ -11,28 +12,67 @@ const navLinks = [
         path: '/blog'
     },
     {
-        title: 'Contact me',
+        title: 'Contact Me',
         path: '/contact-me'
     },
     {
         title: 'Login',
         path: '/login'
     }
-]
+];
 
-export default function Navigation(){
+
+let useCloseOnClickOutside = (userHandler) => {
+    let domNode = useRef();
+
+    useEffect(() =>{
+
+        let handler = (event) =>{
+
+            if(!domNode.current.contains(event.target)){
+                userHandler();
+            }
+        }
+
+        document.addEventListener('mousedown', handler);
+
+        return () => {
+            document.removeEventListener('mousedown', handler);
+        }
+    });
+
+    return domNode;
+}
+
+export default function Navigation({user}){
+
+    const [menuActive, setMenuActive] = useState(false);
+
+    let navBarRef = useCloseOnClickOutside(() => {
+        setMenuActive(false);
+    })
+
     return(
-        <nav className='site-navigation'>
-            <span>My React Blog</span>
-            <ul>
-                {
-                    navLinks.map((link, index) =>{
-                        return(<li key={index}>
-                            <Link to={link.path}>{link.title}</Link>
-                        </li>)
-                    })
-                }
-            </ul>
+        <nav ref={navBarRef} className='site-navigation'>
+            <span className='menu-title'>My React Blog</span>
+            <div className={`menu-content-container ${menuActive && 'active'}`}>
+                <ul>
+                    {
+                        navLinks.map((link, index) =>{
+                            return(<li key={index}>
+                                <Link to={link.path}>{link.title}</Link>
+                            </li>)
+                        })
+                    }
+                </ul>
+
+            <span className='menu-avatar-container'>
+                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" size={38}/>
+                <span className='menu-avatar-name'>{`${user.firstName} ${user.lastName}`}</span>
+            </span>
+            </div>
+
+            <i className='ionicons icon ion-ios-menu' onClick={() => setMenuActive(!menuActive)}/>
         </nav>
     )
 }

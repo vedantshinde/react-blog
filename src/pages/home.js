5415b2bbbfb1c08ccb121a89ae4c96d6d9001db4
Vedant Shinde <vedantshinde101@gmail.com>
2021-08-20 13:11:44 +0530

    Add responsive featured & trending section to the home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import {PostMasonry, MasonryPost} from '../components/common'
+import trending from '../assets/mocks/trending';
+import featured from '../assets/mocks/featured';
+
+const trendingConfig = {
+    1: {
+        gridArea: '1 / 2 / 3/ 3'
+    }
+};
+
+const featuredConfig = {
+    0: {
+        gridArea: '1 / 1 / 2 / 3',
+        height: '300px'
+    },
+    1: {
+        height: '300px',
+    },
+    3: {
+        height: '630px',
+        marginLeft: '30px',
+        width: '630px'
+    }
+}
+
+const mergeStyles = function(posts, config){
+    posts.forEach((post, index) =>{
+        post.style = config[index];
+    })
+}
+
+mergeStyles(trending, trendingConfig);
+mergeStyles(featured, featuredConfig);
+
+// The last featured post is displayed seperately
+const lastFeaturedPost = featured.pop();
 
 export default function Home(){
     return(
-        <div>
-            Home
-        </div>
+        <section className='container home'>
+            <div className='row'>
+                <h1>Featured Posts</h1>
+                <section className='featured-posts-container'>
+                    <PostMasonry posts={featured} columns={2} tagsOnTop={true} />
+                    <MasonryPost post={lastFeaturedPost} tagsOnTop={true} />
+                </section>
+                <h1>Trending Posts</h1>
+                <PostMasonry posts={trending} columns={3} />
+            </div>
+        </section>
     )
 }
